23f796311a02f027b33d9bb7f58ec470be325275
Sujitmaurya123 <sujitkic6802maurya@gmail.com>
2025-01-29 00:26:58 +0530

    allow cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true,limit:'10mb' }));
 app.use(cookieParser());
 const corsOptions={
-    origin:"http://localhost:5173",
+    origin: "*", // Allow all origins
     credentials:true,
 }
 app.use(cors(corsOptions));
