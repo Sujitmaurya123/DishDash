5e7ca9b2e37b31cffafaebce1587db89349b4d61
Sujitmaurya123 <sujitkic6802maurya@gmail.com>
2025-01-29 22:15:48 +0530

    change index.ts and package.js

--- server/src/index.ts
+++ server/src/index.ts
@@ -8,10 +8,11 @@ import orderRoute from "./routes/order.route.js";
 import connectDB from "./db/connectDB.js";
 import bodyPaser from "body-parser"
 import cookieParser from "cookie-parser";
+import path from "path"
 
 const app: Application = express();
 const PORT = process.env.PORT || 3000;
-
+const DIRNAME=path.resolve();
 // * Middleware for any project
 app.use(bodyPaser.json({limit:'10mb'}));
 app.use(express.json());
@@ -28,6 +29,11 @@ app.use("/api/v1/user", userRoute);
 app.use("/api/v1/restaurant", restaurantRoute);
 app.use("/api/v1/menu", menuRoute);
 app.use("/api/v1/order", orderRoute);
+
+app.use(express.static(path.join(DIRNAME,"/client/dist")));
+app.use("*",(_,res)=>{
+    res.sendFile(path.resolve(DIRNAME,"client","dist","index.html"));
+})
 
 app.get("/",(req,res)=>{
     res.send("API Working with /api/v1");
